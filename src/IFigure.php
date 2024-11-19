282d91941c98b1be9ca19eb72f24098285505bb0
Amir Gallyamov <am1r.gallyamov@yandex.ru>
2024-11-19 08:17:34 +0500

    some changes in IFigure.php

--- src/IFigure.php
+++ src/IFigure.php
@@ -1,23 +1,28 @@
 <?php
-require_once('./color.php')
+require_once('./color.php');
 
 
-interface IFigure{
-    public function_construct(Color $color):
+interface IFigure {
+    public function __construct(Color $color);
     public function getColor(): Color;
     public function getIcon(): string;
 }
 
-abstract class Figure implements Figure{
+abstract class Figure implements IFigure {
     private Color $color;
     protected string $icon;
 
+    public function __construct(Color $color){
+        $this->color = $color;
+    }
+
     public function getColor(): Color {
         return $this->color;
+    }
 
     public function getIcon(): string {
-        
+        $prefix = $this->color === Color::Black ? 'b' : 'w';
+        return $prefix . $this->icon;
+    }
 
-    }
-    }
 }
