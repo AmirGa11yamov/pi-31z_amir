b6ec8273f9d37cf386108e046a1f9feb7149f2f4
Amir Gallyamov <am1r.gallyamov@yandex.ru>
2024-11-22 08:33:49 +0500

    added other fugures

---
+++ src/Bishop.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('IFigure.php');
+
+class Bishop extends Figure {
+    protected array $icon = ["\u{265D}", "\u{2657}"];
+}
--- src/IFigure.php
+++ src/IFigure.php
@@ -16,7 +16,10 @@ abstract class Figure implements IFigure {
 
     public function __construct(Color $color){
         $this->color = $color;
-@@ -23,14 +23,20 @@
+    }
+
+    public function getColor(): Color {
+        return $this->color;
     }
 
     public function getIcon(): string {
@@ -36,5 +39,6 @@ abstract class Figure implements IFigure {
 
     public function canAttack(int $from_row, int $from_col, int $to_row, int $to_col, Board $board): bool {
         return false;
+        return $this->canMove($from_row, $from_col, $to_row, $to_col, $board);
     }
 }
